00b848dc3fb0beb0076004ff6bd1331a3823fad3
Sujata <sujatapatel7827@gmail.com>
2026-03-01 02:13:05 +0530

    updated project

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function toggleMenu(){
-  document.getElementById("menu").classList.toggle("active");
+  const menu = document.getElementById("menu");
+  const toggle = document.querySelector(".menu-toggle i");
+
+  menu.classList.toggle("active");
+
+  if(menu.classList.contains("active")){
+    toggle.classList.remove("fa-bars");
+    toggle.classList.add("fa-xmark");
+  } else {
+    toggle.classList.remove("fa-xmark");
+    toggle.classList.add("fa-bars");
+  }
 }
